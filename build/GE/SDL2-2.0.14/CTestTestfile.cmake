15e15ccf8c434859940ee4e7be21d3b077bf7df8
afar0308 <afdhalarrazy111@gmail.com>
2021-01-02 08:55:58 +0700

    Add box2d and tmx dependency

--- build/GE/SDL2-2.0.14/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/afar0308/Documents/GE-Game-Engine/GE/SDL2-2.0.14
-# Build directory: /home/afar0308/Documents/GE-Game-Engine/build/GE/SDL2-2.0.14
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
